40670c3ce0984d53ee5ee0e6d7ada94de26cb4e0
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2022-05-13 11:15:32 +0900

    add: vuex

---
+++ section9_vuex/vuex/src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <!-- <button @click="increment">+</button> -->
+    <button @click="incrementAction">+</button>
+    <button @click="addCount">+10</button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods:{
+    // increment(){
+    //   this.$store.commit('increment')
+    // },
+    // addCount(){
+    //   this.$store.commit('addCount', { 
+    //     value: 10
+    //    })
+    // }
+    ...mapActions(['incrementAction', 'addCountAction']),
+    // 上と下は同じ
+    // increment(){
+    //   this.$store.dispatch('incrementAction')
+    // },
+
+    // mapActionsで引数なしならこの3行と同じ意味
+    // incrementAction(){
+    //   this.$store.dispatch('incrementAction')
+    // },
+
+    // mapActionsでこのコードが生成される
+    // これだと引数を渡せない
+    // addCountAction(){
+    //   this.$store.dispatch('addCountAction')
+    // },
+    
+    // 引数をつける場合は、他の関数からアクションを呼び出す
+    // addCount(){
+    //   this.$store.dispatch('addCountAction', {
+    //     value: 10
+    //   }),
+
+    // mapActionsで引数を取る場合は
+    // 別メソッドを用意し、this.action名(引数)でOK
+    addCount(){
+      this.addCountAction( {
+        value: 10
+      })
+    }
+  }
+}
+</script>
+
